154577d1ab375eb898da2de23304914288d12c11
Jimmy Peleha <jimmy.peleha@entelect.co.za>
2018-08-18 19:56:25 +0200

    Added style

--- coming-soon/js/custom.js
+++ coming-soon/js/custom.js
@@ -1,3 +1,25 @@
 function showSuccessToast() {
-    toastr.success('Message sent!', 'We\'ll get back to you soon :)')
+    setToastOptions();
+    toastr.success('Message sent!', 'We\'ll get back to you soon :)');
+    $("#emailAddress").val("");
+}
+
+function setToastOptions() {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+      };
 }
